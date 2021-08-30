440779c50ae8da7303f1b6ee64985e89bf579eff
Antonio Randazzo <89759999+arandazz@users.noreply.github.com>
2021-08-30 08:27:12 -0400

    Add files via upload
    missed files on import

---
+++ MindexInterview/code-challenge.Tests/ReportingControllerTests.cs
@@ -0,0 +1,61 @@
+using challenge.Controllers;
+using challenge.Data;
+using challenge.Models;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using code_challenge.Tests.Integration.Extensions;
+
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using code_challenge.Tests.Integration.Helpers;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace code_challenge.Tests.Integration
+{
+    [TestClass]
+    public class ReportingControllerTests
+    {
+        private static HttpClient _httpClient;
+        private static TestServer _testServer;
+
+        [ClassInitialize]
+        public static void InitializeClass(TestContext context)
+        {
+            _testServer = new TestServer(WebHost.CreateDefaultBuilder()
+                .UseStartup<TestServerStartup>()
+                .UseEnvironment("Development"));
+
+            _httpClient = _testServer.CreateClient();
+        }
+
+        [ClassCleanup]
+        public static void CleanUpTest()
+        {
+            _httpClient.Dispose();
+            _testServer.Dispose();
+        }
+
+        [TestMethod]
+        public void GetDirectReportsByEmployeeById_Returns_ReportingStructure()
+        {
+            // Arrange
+            var employeeId = "16a596ae-edd3-4847-99fe-c4518e82c86f";
+
+            // Execute
+            var getRequestTask = _httpClient.GetAsync($"api/employeeReports/{employeeId}");
+            var response = getRequestTask.Result;
+
+            // Assert
+            var directReports = response.DeserializeContent<ReportingStructure>();
+            Assert.AreEqual(4, directReports.NumberOfReports);
+        }
+    }
+}
